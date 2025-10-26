f4a9df9c3624fe00b5e3beaee9f37df379a03b76
sw4ra <149519611+swarasprogram@users.noreply.github.com>
2025-10-26 14:01:35 +0530

    Updated vite.config.js and added vercel.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: './', // ensures assets load correctly in production
 })
